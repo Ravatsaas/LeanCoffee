8cfde5d95dc46624f250d6d4754a1fd714986b30
Ruben Ravatsaas <rubrav@gmail.com>
2020-02-23 14:56:15 -0600

    Clean up old styles

--- src/App.vue
+++ src/App.vue
@@ -33,19 +33,6 @@ export default class App extends Vue {}
 <style>
 @import url('https://fonts.googleapis.com/css?family=Lobster|Roboto&display=swap');
 
-/* body {
-  background-color: #eee;
-} */
-
-/*#app {
-  font-family: Roboto, Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-
 h1 {
   font-family: 'Lobster', cursive;
   font-size: 3em;
